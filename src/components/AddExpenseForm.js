7b84d9ae8c49b20525e0f3c98977d3635f7f9c9a
danielacsion <danielasion629@gmail.com>
2025-04-12 05:34:38 -0700

    Fifth Commit of the Week: finished expense form

--- src/components/AddExpenseForm.js
+++ src/components/AddExpenseForm.js
@@ -12,4 +12,18 @@ const AddExpenseForm = ({ onAddExpense }) => {
         setCategory('');
         setDate('');
     };
-}
+
+    return (
+        <form onSubmit={handleSubmit} style={{ margin: '1rem 0' }}>
+          <input type="number" placeholder="Amount" value={amount} onChange={(e) => setAmount(e.target.value)} />
+          <select value={category} onChange={(e) => setCategory(e.target.value)}>
+            <option value="">Select Category</option>
+            <option value="Groceries">Groceries</option>
+            <option value="Rent">Rent</option>
+          </select>
+          <input type="date" value={date} onChange={(e) => setDate(e.target.value)} />
+          <button type="submit">Add Expense</button>
+        </form>
+      );
+    };
+export default AddExpenseForm;
