8f6c77544e848de65c88fabfb1e3605bc4cc39a4
danielacsion <danielasion629@gmail.com>
2025-04-20 15:05:57 -0700

    header is finished

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,42 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Header = () => {
-   
+  return (
+    <nav style={{
+      display: 'flex',
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: '1rem',
+      backgroundColor: '#007BFF',
+      color: '#fff',
+      fontSize: '1.2rem',
+      boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+    }}>
+      <Link
+        to="/"
+        style={{
+          textDecoration: 'none',
+          color: '#fff',
+          margin: '0 1.5rem',
+          fontWeight: 'bold',
+        }}
+      >
+        Home
+      </Link>
+      <Link
+        to="/expenses"
+        style={{
+          textDecoration: 'none',
+          color: '#fff',
+          margin: '0 1.5rem',
+          fontWeight: 'bold',
+        }}
+      >
+        Expenses
+      </Link>
+    </nav>
+  );
 };
 
 export default Header;
